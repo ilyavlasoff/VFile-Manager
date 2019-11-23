f091f6b0bedaa83ca352ed5c38de4863e5821301
Ilya <vlasov.vlasov2015@gmail.com>
2019-11-24 01:41:52 +0300

    Added classes for manipulating dir contents, settings form

---
+++ VFile Manager/FileOperator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace VFile_Manager
+{
+    static class FileOperator
+    {
+        public enum Side { Left, Right }
+        public static bool DirFirst { get; set; }
+
+        public static void InitilalDirs()
+        {
+            List<String> fileStartPaths = SavedDataReader.GetSavedStartingDirsFromXml().ToList();
+            String leftPath = fileStartPaths[0];
+            if (!Directory.Exists(leftPath))
+            {
+                leftPath = Directory.GetCurrentDirectory();
+                SavedDataReader.SetSavedStartingDirsToXml(leftPath, Side.Left);
+            }
+            String rightPath = fileStartPaths[1];
+            if (!Directory.Exists(rightPath))
+            {
+                rightPath = Directory.GetCurrentDirectory();
+                SavedDataReader.SetSavedStartingDirsToXml(rightPath, Side.Right);
+            }
+            FileContainers.FileDualContainer.ChooseContainer(Side.Left).GoToDirectory(leftPath);
+            FileContainers.FileDualContainer.ChooseContainer(Side.Right).GoToDirectory(rightPath);
+        }
+
+        public static IEnumerable<FileObjects.IFileObject> GetDirContainsList(Side _s)
+        {
+            return FileContainers.FileDualContainer.ChooseContainer(_s).DirContent;
+        }
+
+        public static IEnumerable<String> GetAllLogicalDrives()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            IEnumerable<String> allDrivesName = allDrives.Select((item) => item.Name);
+            return allDrivesName;
+        }
+
+        public static void HandleOpenFileOrDir(object _file, Side _s)
+        {
+            if (_file is FileObjects.Dir)
+            {
+                FileObjects.Dir choosedDir = _file as FileObjects.Dir;
+                FileContainers.FileDualContainer.ChooseContainer(_s).GoToDirectory(choosedDir);
+            }
+            else if (_file is FileObjects.File)
+            {
+                (_file as FileObjects.File).Open();
+            }
+            else
+            {
+                throw new Exception("Unable to convert object to dir or file type");
+            }
+        }
+
+        public static void NavigateToPreviousDirectory(Side _s)
+        {
+            try
+            {
+                FileObjects.Dir parentDir = FileContainers.FileDualContainer.ChooseContainer(_s).Path.GetParentDirectory();
+                if (parentDir != null)
+                {
+                    FileContainers.FileDualContainer.ChooseContainer(_s).GoToDirectory(parentDir);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ VFile Manager/SavedDataReader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.IO;
+using System.Xml;
+namespace VFile_Manager
+{
+    static class SavedDataReader
+    {
+        private static String saved_path = "saved_paths.xml";
+        public static IEnumerable<String> GetSavedStartingDirsFromXml()
+        {
+            XDocument savedData = null;
+            try
+            {
+                savedData = XDocument.Load(saved_path);
+            }
+            catch (FileNotFoundException ex)
+            {
+                savedData = new XDocument();
+                XElement root = new XElement("Paths");
+                XElement leftDirPath = new XElement("left");
+                leftDirPath.Value = Directory.GetCurrentDirectory();
+                XElement rightDirPath = new XElement("right");
+                rightDirPath.Value = Directory.GetCurrentDirectory();
+                root.Add(leftDirPath, rightDirPath);
+                savedData.Add(root);
+                savedData.Save(saved_path);
+                return new List<String> { Directory.GetCurrentDirectory(), Directory.GetCurrentDirectory() };
+            }
+            try
+            {
+                XElement root = savedData.Root;
+                String leftPath = root.Element("left").Value;
+                String rightPath = root.Element("right").Value;
+                return new List<String> { leftPath, rightPath };
+            }
+            catch (XmlException ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static void SetSavedStartingDirsToXml(String _path, FileOperator.Side _s)
+        {
+            if (_path == String.Empty)
+                _path = Directory.GetCurrentDirectory();
+            try
+            {
+                XDocument savedData = XDocument.Load(saved_path);
+                XElement root = savedData.Root;
+                String side = (_s == FileOperator.Side.Left) ? "left" : "right";
+                root.Element(side).Value = _path;
+                savedData.Add(root);
+                savedData.Save(saved_path);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
