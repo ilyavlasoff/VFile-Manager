26899b7fc38015abd491ef02c8abd84a0e74e0dc
Ilya <vlasov.vlasov2015@gmail.com>
2019-11-24 01:36:40 +0300

    Added classes for store directories content

---
+++ VFile Manager/File Containers/FileContainer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace VFile_Manager.File_Containers
+{
+    class FileContainer
+    {
+        public FileObjects.Dir Path { get; private set; }
+
+        public List<FileObjects.IFileObject> DirContent { get; private set; }
+
+        public FileContainer(String _path)
+        {
+            if (Directory.Exists(_path))
+            {
+                Path = new FileObjects.Dir(_path);
+            }
+        }
+
+        public FileContainer()
+        {
+            DirContent = new List<FileObjects.IFileObject>();
+        }
+
+        public void GoToDirectory(String _directoryName)
+        {
+            FileObjects.Dir dinfo = new FileObjects.Dir(_directoryName);
+            GoToDirectory(dinfo);
+        }
+
+        public void GoToDirectory(FileObjects.Dir _directory)
+        {
+            if (_directory.Exists)
+            {
+                Path = _directory;
+                DirContent.Clear();
+                try
+                {
+                    IEnumerable<FileObjects.File> files = Path.GetFiles();
+                    DirContent.AddRange(files);
+                }
+                catch (Exception ex)
+                {
+
+                }
+                try
+                {
+                    IEnumerable<FileObjects.Dir> dirs = Path.GetDirectories();
+                    DirContent.AddRange(dirs);
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            }
+        }
+
+    }
+}
---
+++ VFile Manager/File Containers/FileDualContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VFile_Manager.File_Containers
+{
+    static class FileDualContainer
+    {
+        private static FileContainer FC_Left = new FileContainer(), FC_Right = new FileContainer();
+        public static FileContainer ChooseContainer(FileOperator.Side s)
+        {
+            if (s == FileOperator.Side.Left)
+                return FC_Left;
+            else
+                return FC_Right;
+        }
+    }
+}
